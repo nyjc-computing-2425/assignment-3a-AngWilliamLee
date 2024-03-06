814d96341609606d7aac7650f60d8c552379e324
AngWilliamLee <160611372+AngWilliamLee@users.noreply.github.com>
2024-03-06 05:39:11 +0000

    1st attempt

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 nric = input('Enter an NRIC number: ')
 
 # Type your code below
+
+if nric.startswith('T' or 'G'): #doesn't work for more than  1 argument
+  print("prefix valid")
+
+digits = nric[1:8]
+if digits.isdigit():
+  print("digits are valid")
+
+suffix = nric[-1]
+if suffix.isalpha():
+  print("suffix valid")
